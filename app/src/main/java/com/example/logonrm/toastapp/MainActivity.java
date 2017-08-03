18415ff231402f4eba8b954ab20436f208d5e08b
logonrm <logonrm@fiap.com.br>
2017-08-02 22:53:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/toastapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.logonrm.toastapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.logonrm.mylibrary.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomToast customToast = new CustomToast();
+        if (BuildConfig.REPORT_CRASH)
+            customToast.showToast(this, "Report Crash");
+        else
+            customToast.showToast(this, "Not Report Crash");
+    }
+}
